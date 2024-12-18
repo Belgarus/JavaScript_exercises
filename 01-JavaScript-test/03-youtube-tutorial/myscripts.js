013b508b8a2380c4c08f07aeb920a1ba1cec467e
Belgarus <179312058+Belgarus@users.noreply.github.com>
2024-12-18 18:04:31 +0100

    added if and else colores

--- 01-JavaScript-test/03-youtube-tutorial/myscripts.js
+++ 01-JavaScript-test/03-youtube-tutorial/myscripts.js
@@ -2,5 +2,9 @@ var button = document.querySelector('button');
 var box = document.getElementById('changeMe');
 
 button.onclick = function changeColor(){
-    box.style.background = 'blue';
-}
+    if(box.style.background == 'red'){
+        box.style.background = 'blue';
+        }else{
+            box.style.background = 'red';
+        }
+} 
