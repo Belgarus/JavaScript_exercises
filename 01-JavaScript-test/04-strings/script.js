4ead9180b63f95fea90eb622ace10453077a7dec
Belgarus <179312058+Belgarus@users.noreply.github.com>
2024-12-19 20:21:21 +0100

    added more strings

--- 01-JavaScript-test/04-strings/script.js
+++ 01-JavaScript-test/04-strings/script.js
@@ -1,6 +1,9 @@
 const greeting = "Hello" + ", " + "World";
 console.log(greeting)
 
+const hello = `${"Hello"}, ${"World"}`;
+console.log(hello)
+
 const name = "Chris";
 const greet = `Hello, ${name}`;
 console.log(greet)
@@ -8,3 +11,12 @@ console.log(greet)
 const multiline = `Zeile 1
 Zeile 2`;
 console.log(multiline)
+
+const escape = 'I\'m here!';
+console.log(escape)
+
+const multiline2 = "Zeile 1\nZeile 2";
+console.log(multiline2)
+
+const result = "Score: " + 100; // "Score: 100"
+console.log(result)
