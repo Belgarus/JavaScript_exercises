d13d4ea7ad1f3e111ba44371c97656cc3a9c86f8
Belgarus <179312058+Belgarus@users.noreply.github.com>
2025-01-18 19:00:33 +0100

    added programm if u press button and input red, color of the header == red

--- 06-strings-test/script.js
+++ 06-strings-test/script.js
@@ -1,7 +1,9 @@
+//text length section
 let ABC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 document.getElementById("txt").innerHTML = ABC
 document.getElementById("txtlnght").innerHTML = ABC.length
 
+//alert section
 let year = prompt('In which year was ECMAScript-2015 specification published?', '');
 
 if (year == 2015) {
@@ -9,4 +11,13 @@ if (year == 2015) {
     alert( "You're so smart!" );
   } else {
     alert("That's not correct :(");
+}
+
+//if/else color section
+function pressed() {
+    var text = document.getElementById("inp").value;
+
+    if (text == "red") {
+    document.getElementById("header").style.color = "red";
+    }
 }
