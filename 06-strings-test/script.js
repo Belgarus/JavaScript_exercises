72bdd993b6e2dad222d741772930b1d99acd63c8
Belgarus <179312058+Belgarus@users.noreply.github.com>
2025-01-18 19:14:52 +0100

    added a lot of if else statements

--- 06-strings-test/script.js
+++ 06-strings-test/script.js
@@ -17,9 +17,27 @@ if (year == 2015) {
 function pressed() {
     var text = document.getElementById("inp").value;
 
-    if (text == "red") {
-    document.getElementById("header").style.color = "red";
-    } else {
+    if (text === "red") {
+        document.getElementById("header").style.color = "red";
+    } else if (text === "yellow"){
+        document.getElementById("header").style.color = "yellow";
+    }  else if (text === "blue"){
+        document.getElementById("header").style.color = "blue";
+    }else if (text === "green"){
+        document.getElementById("header").style.color = "green";
+    }else if (text === "orange"){
+        document.getElementById("header").style.color = "orange";
+    }else if (text === "brown"){
+        document.getElementById("header").style.color = "brown";
+    }else if (text === "pink"){
+        document.getElementById("header").style.color = "pink";
+    }else if (text === "purple"){
+        document.getElementById("header").style.color = "purple";
+    }else if (text === "black"){
+        document.getElementById("header").style.color = "black";
+    }else if (text === "white"){
+        document.getElementById("header").style.color = "white";
+    }else{
         document.getElementById("header").style.color = "rgb(193, 193, 193)";
     }
 }
