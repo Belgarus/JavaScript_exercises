a68c8060beef57493bcaeccb4ed757fe6699e3e2
Belgarus <179312058+Belgarus@users.noreply.github.com>
2025-01-18 18:46:07 +0100

    added if else statements

--- 06-strings-test/script.js
+++ 06-strings-test/script.js
@@ -1,3 +1,12 @@
 let ABC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 document.getElementById("txt").innerHTML = ABC
 document.getElementById("txtlnght").innerHTML = ABC.length
+
+let year = prompt('In which year was ECMAScript-2015 specification published?', '');
+
+if (year == 2015) {
+    alert( "That's correct!" );
+    alert( "You're so smart!" );
+  } else {
+    alert("That's not correct :(");
+}
