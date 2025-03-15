880f6a90a44460a87030e54a223db86b62eb199f
Belgarus <179312058+Belgarus@users.noreply.github.com>
2025-03-15 19:53:33 +0100

    phyton test

--- main.py
+++ main.py
@@ -2,4 +2,7 @@ print("hello world")
 print("dlrow olleh")
 
 if 5 > 2:
-    print("Five is greater than 2")
+    print("Five is greater than two!")
+
+if 5 > 2:
+        print("Five is greater than two!") 
