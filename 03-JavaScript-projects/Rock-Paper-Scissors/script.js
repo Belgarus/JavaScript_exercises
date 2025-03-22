94b2f5a8a3cbdd03cd8495e7e6e760a7686ab1f0
Belgarus <179312058+Belgarus@users.noreply.github.com>
2025-03-22 18:38:02 +0100

    working on the score

--- 03-JavaScript-projects/Rock-Paper-Scissors/script.js
+++ 03-JavaScript-projects/Rock-Paper-Scissors/script.js
@@ -12,44 +12,55 @@ function getHumanChoice() {
 
   let humanScore = 0;
   let computerScore = 0;
+  let x, y = 0
 
   function playGame() {
 
-let incHumanScore = (humanScore) => humanScore + 1;
-console.log(incHumanScore(humanScore))
+let incHumanScore = (humanScore) => humanScore++;
+//console.log("Human Score:" + incHumanScore(humanScore))
+
+let incComputerScore = (computerScore) => computerScore++;
+//console.log("Human Score:" + incComputerScore(computerScore))
+
+
 function playRound(humanChoice, computerChoice) {
 
     console.log("Human Choice:" + " " + humanChoice);
     console.log("Computer Choice:" + " " + computerChoice);
 
     if (humanChoice === "Rock" && computerChoice === "Scissors"){
-        return "You won! Rock crushes Scissors."; 
+        incHumanScore;
+        return "You won! Rock crushes Scissors.";
     } else if (humanChoice === "Rock" && computerChoice === "Paper") {
+        incComputerScore;
         return "You lost! Rocks get wrapped by Papers."
     } else if (humanChoice === "Rock" && computerChoice === "Rock") {
         return "Patt, Rock and Rock!" //HumanChoice = Rock
     } else if (humanChoice === "Paper" && computerChoice === "Rock") {
+        incHumanScore;
         return "You won! Papers wrap Rocks." 
     } else if (humanChoice === "Paper" && computerChoice === "Paper") {
         return "Patt, Paper and Paper!"
     } else if (humanChoice === "Paper" && computerChoice === "Scissors") {
+        incComputerScore;
         return "You lost! Paper gets cut by Scissors." //HumanChoice = Paper
     } else if (humanChoice === "Scissors" && computerChoice === "Rock") {
+        incComputerScore;
         return "You lost! Scissors get crushed by Rocks."
     } else if (humanChoice === "Scissors" && computerChoice === "Paper") {
+        incHumanScore;
         return "You won! Scissorss cut Paper." 
     } else if (humanChoice === "Scissors" && computerChoice === "Scissors") {
         return "Patt, Scissors and Scissors." //HumanChoice = Scissors
     } else {
         return "invalid input"
-    }
+    };
+
     
   }
+  console.warn("Human Score: " + humanScore)
+    console.warn("Computer Score: " + computerScore)
+  console.log(playRound(getHumanChoice(), getComputerChoice()));
   }
 
-  
-  console.log(playRound(getHumanChoice(), getComputerChoice()))
-  
-
-
-  
+console.log(playGame())
