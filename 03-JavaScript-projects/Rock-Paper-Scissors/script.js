4dedea55b133841df3eb010a19fe7451de114d03
Belgarus <179312058+Belgarus@users.noreply.github.com>
2025-03-22 11:00:34 +0100

    added more win options

--- 03-JavaScript-projects/Rock-Paper-Scissors/script.js
+++ 03-JavaScript-projects/Rock-Paper-Scissors/script.js
@@ -24,9 +24,13 @@ function playRound(humanChoice, computerChoice) {
 
     console.log("Human Choice:" + " " + humanChoice)
     console.log("Computer Choice:" + " " + computerChoice)
-    
+
     if (humanChoice === "Rock" && computerChoice === "Scissors"){
         return "You won! Rock defeats Scissors."
+    } else if (humanChoice === "Rock" && computerChoice === "Paper") {
+        return "You lost! Rocks get defeated by Papers"
+    } else if (humanChoice === "Rock" && computerChoice === "Rock") {
+        return "Patt, Rock and Rock!"
     } else {
         return "invalid input"
     }
