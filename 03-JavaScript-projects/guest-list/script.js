6f35bc9d30c26e8f568e7dbc74999dacb89e6b0d
Belgarus <179312058+Belgarus@users.noreply.github.com>
2025-04-14 17:47:02 +0200

    fixed a spelling mistake and completed the exercise

--- 03-JavaScript-projects/guest-list/script.js
+++ 03-JavaScript-projects/guest-list/script.js
@@ -1,18 +1,14 @@
 const people = ['Chris', 'Anne', 'Colin', 'Terri', 'Phil', 'Lola', 'Sam', 'Kay', 'Bruce'];
 
-const admitted = document.querySelector('.admitted');
-const refused = document.querySelector('.refused');
+const admitted = document.getElementById('admitted');
+const refused = document.getElementById('refused');
 admitted.textContent = 'Admit: ';
 refused.textContent = 'Refuse: ';
-guest = ""
 
 // loop starts here
-for (i = 0; i > people.length; i++)
-if (people !== "Phil" && people !== "Lola"){
-        admitted.textContent += people[i] + '';
+for (i = 0; i < people.length; i++){
+if (people[i] !== "Phil" && people[i] !== "Lola"){
+        admitted.textContent += people[i] + ', ';
 } else {
-    refused.textContent += people[i]
-}
-
-// refused.textContent += ;
-// admitted.textContent += ;
+    refused.textContent += people[i] + ', ';
+}}
