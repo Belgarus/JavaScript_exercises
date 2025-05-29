ac778c26c2a957dc66e1925c186e1722f68a81ab
Belgarus <179312058+Belgarus@users.noreply.github.com>
2025-05-29 12:12:42 +0200

    not finished jet but added first filters

--- 04-JavaScript-Loops-Arrays/03-filter-range-in-place/filter-range-in-place.js
+++ 04-JavaScript-Loops-Arrays/03-filter-range-in-place/filter-range-in-place.js
@@ -1,9 +1,10 @@
-let arr = ['a', 'b', 'd', 'e', 'k', 'z']
-let b = "b"
-let e = "e"
-
-function filterRangeInPlace(arr, b, e) {
-
+function filterRangeInPlace(arr, a, b) {
+    for(i = 0; i > arr.lenght; i++){
+         if (arr[i] < a || arr[i] > b){
+            arr.splice(arr[i])
+         }
+    }
 }
-
-console.log(arr)
+let arr = [5, 3, 8, 1];
+filterRangeInPlace(arr, 1, 4); // removed the numbers except from 1 to 4
+console.log( arr ); // [3, 1]
