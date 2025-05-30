fdd99fcc274767b3d3b8e0ca48536159cf71b127
Belgarus <179312058+Belgarus@users.noreply.github.com>
2025-05-30 11:12:34 +0200

    simplefied it even more

--- 04-JavaScript-Loops-Arrays/03-filter-range-in-place/filter-range-in-place.js
+++ 04-JavaScript-Loops-Arrays/03-filter-range-in-place/filter-range-in-place.js
@@ -1,8 +1,7 @@
 function filterRangeInPlace(arr, a, b) {
   for (let i = 0; i < arr.length; i++) {
-    let val = arr[i];
     // remove if outside of the interval
-    if (val < a || val > b) {
+    if (arr[i] < a || arr[i] > b) {
       arr.splice(i, 1);
       i--;
     }
