21b98d8532f719f44eec10aa93aabfaf409d2ec3
Belgarus <179312058+Belgarus@users.noreply.github.com>
2025-05-30 23:18:30 +0200

    added the basics

---
+++ 04-JavaScript-Loops-Arrays/04-copy-sort/copy-sort.js
@@ -0,0 +1,9 @@
+function copySorted(arr) {
+
+}
+
+let arr = ["HTML", "JavaScript", "CSS"];
+let sorted = copySorted(arr);
+
+alert( sorted ); // CSS, HTML, JavaScript
+alert( arr ); // HTML, JavaScript, CSS (no changes)
