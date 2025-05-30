dc9f6295a049018371ae81db211c7454886706ae
Belgarus <179312058+Belgarus@users.noreply.github.com>
2025-05-30 11:09:41 +0200

    fixing last errors

--- 04-JavaScript-Loops-Arrays/03-filter-range-in-place/filter-range-in-place.js
+++ 04-JavaScript-Loops-Arrays/03-filter-range-in-place/filter-range-in-place.js
@@ -1,10 +1,13 @@
 function filterRangeInPlace(arr, a, b) {
-    for(i = 0; i > arr.lenght; i++){
-         if (arr[i] < a || arr[i] > b){
-            arr.splice(arr[i])
+    for(i = 0; i < arr.lenght; i++){
+        let val = arr[i];
+         if (val < a || val > b){ // remove if outside of the interval
+            arr.splice(i, 1);
+            i--;
          }
     }
 }
+
 let arr = [5, 3, 8, 1];
 filterRangeInPlace(arr, 1, 4); // removed the numbers except from 1 to 4
 console.log( arr ); // [3, 1]
