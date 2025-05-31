bccaa0c7e91f269d81da52ca4be2c782e7983e9b
Belgarus <179312058+Belgarus@users.noreply.github.com>
2025-05-31 22:39:39 +0200

    made a fullname function

---
+++ 04-JavaScript-Loops-Arrays/06-map-to-object/map-to-object.js
@@ -0,0 +1,12 @@
+let john = { name: "John", surname: "Smith", id: 1 };
+let pete = { name: "Pete", surname: "Hunt", id: 2 };
+let mary = { name: "Mary", surname: "Key", id: 3 };
+
+function fullname(arr){
+ return arr.name + " " + arr.surname;
+}
+console.log(fullname(john))
+
+let usersMapped = [
+    {}
+]
