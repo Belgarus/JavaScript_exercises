1ea3c45263efbc65ece26b93e302228f0f78f7f4
Belgarus <179312058+Belgarus@users.noreply.github.com>
2025-06-02 16:31:17 +0200

    removed the comments

--- 04-JavaScript-Loops-Arrays/11-create-keyed-obj-from-array/create-keyed-obj-from-array.js
+++ 04-JavaScript-Loops-Arrays/11-create-keyed-obj-from-array/create-keyed-obj-from-array.js
@@ -13,13 +13,3 @@ let users = [
 
 let usersById = groupById(users);
 console.log(usersById)
-
-/*
-// after the call we should have:
-
-usersById = {
-  john: {id: 'john', name: "John Smith", age: 20},
-  ann: {id: 'ann', name: "Ann Smith", age: 24},
-  pete: {id: 'pete', name: "Pete Peterson", age: 31},
-}
-*/
