882cbb8ac7e6cab84a9666d11ec1fce0d83e33f9
Belgarus <179312058+Belgarus@users.noreply.github.com>
2025-06-02 15:12:40 +0200

    added the basics

---
+++ 04-JavaScript-Loops-Arrays/09-get-average-age/get-average-age.js
@@ -0,0 +1,11 @@
+function getAverageAge(users){
+
+}
+
+let john = { name: "John", age: 25 };
+let pete = { name: "Pete", age: 30 };
+let mary = { name: "Mary", age: 29 };
+
+let arr = [ john, pete, mary ];
+
+console.log(getAverageAge(arr) ); // (25 + 30 + 29) / 3 = 28
