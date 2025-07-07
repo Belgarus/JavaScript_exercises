248d985c389f7801eb4cd97b272f57dccfcd8717
Belgarus <179312058+Belgarus@users.noreply.github.com>
2025-07-07 12:53:09 +0200

    fixed the getChange test and removed the module

---
+++ 05-TDD/01-vending-machine/script.test.js
@@ -0,0 +1,14 @@
+// This is what a simple unit test looks like:
+test('This sample test should always pass!', function(assert) {
+  var result = 1 + 1;
+  assert.equal(result, 2); // just so we know everything loaded ok
+});
+// A failing test will be RED:
+test('This is what a failing test looks like!', function(assert) {
+  var result = [1,2,3].indexOf(1);  // this should be 0
+  assert.equal(result, -1); // we *expect* this to fail
+});
+test("test getChange", function(assert){
+  var result = getChange(215, 300);
+  assert.deepEqual(result, [50, 20, 10, 5]);
+});
