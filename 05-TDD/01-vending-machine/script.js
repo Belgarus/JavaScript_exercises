7e60f52d889fd432f9680729dbc28ac3148af627
Belgarus <179312058+Belgarus@users.noreply.github.com>
2025-07-07 12:04:27 +0200

    moved the index script to the two seperate files

---
+++ 05-TDD/01-vending-machine/script.js
@@ -0,0 +1,21 @@
+const items = [{"id": 1, "price": 199},
+                      {"id": 2, "price": 129},
+                      {"id": 3, "price": 100}
+       ]
+     function totalPayable(item){
+        
+      }
+      
+      function getChange(totalPayable, cashPaid){
+        const Change = [1000, 500, 200, 100, 50, 20, 10, 5, 2, 1]
+        if(cashPaid < totalPayable){
+            return "Insert more money."
+        } else {
+        const changeMoney = cashPaid - totalPayable
+        for (let i = 0; i < Change.length; i++){
+        change
+        }
+        }
+
+    }
+    console.log(getChange(totalPayable, cashPaid))
