b5e2000e7ca5f6b7485811922cf42b3113cde784
Belgarus <179312058+Belgarus@users.noreply.github.com>
2025-07-07 12:52:36 +0200

    fixed the getChange function

--- 05-TDD/01-vending-machine/script.js
+++ 05-TDD/01-vending-machine/script.js
@@ -1,21 +1,15 @@
-const items = [{"id": 1, "price": 199},
-                      {"id": 2, "price": 129},
-                      {"id": 3, "price": 100}
-       ]
-     function totalPayable(item){
-        
-      }
-      
-      function getChange(totalPayable, cashPaid){
-        const Change = [1000, 500, 200, 100, 50, 20, 10, 5, 2, 1]
-        if(cashPaid < totalPayable){
-            return "Insert more money."
-        } else {
-        const changeMoney = cashPaid - totalPayable
-        for (let i = 0; i < Change.length; i++){
-        change
-        }
-        }
-
+function getChange(totalPayable, cashPaid) {
+  const COINS = [1000, 500, 200, 100, 50, 20, 10, 5, 2, 1];
+  if (cashPaid < totalPayable) {
+    return "Insert more money.";
+  }
+  let change = [];
+  let remaining = cashPaid - totalPayable;
+  for (let i = 0; i < COINS.length; i++) {
+    while (remaining >= COINS[i]) {
+      change.push(COINS[i]);
+      remaining -= COINS[i];
     }
-    console.log(getChange(totalPayable, cashPaid))
+  }
+  return change;
+}
