758eb35b8018d00db0166972f0958224ed4af4b4
Belgarus <179312058+Belgarus@users.noreply.github.com>
2025-07-08 11:17:54 +0200

    added another test (c&p)

--- 05-TDD/01-vending-machine/script.test.js
+++ 05-TDD/01-vending-machine/script.test.js
@@ -14,3 +14,9 @@ test('getChange(486, 600) should equal [100, 10, 2, 2]', function(assert) {
   var expected = [100, 10, 2, 2];
   assert.deepEqual(result, expected);
 })
+
+test('getChange(12, 400) should return [200, 100, 50, 20, 10, 5, 2, 1]', function(assert) {
+  var result = getChange(12, 400);
+  var expected = [200, 100, 50, 20, 10, 5, 2, 1];
+  assert.deepEqual(result, expected);
+});
