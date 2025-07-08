f15e02d6a23b98df58d90cce425e05ce34a5aac4
Belgarus <179312058+Belgarus@users.noreply.github.com>
2025-07-08 11:37:54 +0200

    update script.js by removing the is less money check

--- 05-TDD/01-vending-machine/script.js
+++ 05-TDD/01-vending-machine/script.js
@@ -1,14 +1,11 @@
 function getChange(totalPayable, cashPaid) {
-  const COINS = [1000, 500, 200, 100, 50, 20, 10, 5, 2, 1];
-  if (cashPaid < totalPayable) {
-    return "Insert more money.";
-  }
-  let change = [];
-  let remaining = cashPaid - totalPayable;
-  for (let i = 0; i < COINS.length; i++) {
-    while (remaining >= COINS[i]) {
-      change.push(COINS[i]);
-      remaining -= COINS[i];
+    const COINS = [1000, 500, 200, 100, 50, 20, 10, 5, 2, 1];
+    let change = [];
+    let remaining = cashPaid - totalPayable;
+    for (let i = 0; i < COINS.length; i++) {
+        while (remaining >= COINS[i]) {
+            change.push(COINS[i]);
+            remaining -= COINS[i];
     }
   }
   return change;
