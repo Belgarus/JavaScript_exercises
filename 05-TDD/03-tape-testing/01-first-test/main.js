5515308a31268bb319130946529537c6a83e16c4
Belgarus <179312058+Belgarus@users.noreply.github.com>
2025-07-10 12:59:51 +0200

    fixed the required function name

--- 05-TDD/03-tape-testing/01-first-test/main.js
+++ 05-TDD/03-tape-testing/01-first-test/main.js
@@ -1,7 +1,5 @@
 var test = require('tape').test;
-var test = require('./functions').test;
-
-const add = (a, b) => a + b;
+var add = require('./functions');
 
 test('add: shoud add two numbers correctly', function(t){
     var actual = add(1, 2);
