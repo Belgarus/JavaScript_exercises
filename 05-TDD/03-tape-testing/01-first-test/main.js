449f762c76ad71a9853bda8fe93352c19ba52038
Belgarus <179312058+Belgarus@users.noreply.github.com>
2025-07-10 12:47:45 +0200

    wrote a test that tests the addition of two numbers

--- 05-TDD/03-tape-testing/01-first-test/main.js
+++ 05-TDD/03-tape-testing/01-first-test/main.js
@@ -1,2 +1,10 @@
 var test = require('tape').test;
-test('add')
+
+const add = (a, b) => a + b;
+
+test('add: shoud add two numbers correctly', function(t){
+    var actual = add(1, 2);
+    var expected = 3;
+    t.equal(actual, expected);
+    t.end();
+});
