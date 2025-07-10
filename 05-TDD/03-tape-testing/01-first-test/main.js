940ba9ac57163013292fb24361202eacc6335192
Belgarus <179312058+Belgarus@users.noreply.github.com>
2025-07-10 13:35:56 +0200

    update main.js

--- 05-TDD/03-tape-testing/01-first-test/main.js
+++ 05-TDD/03-tape-testing/01-first-test/main.js
@@ -1,9 +1,9 @@
 var test = require('tape').test;
 var add = require('./functions');
 
-test('add: shoud add two numbers correctly', function(t){
+test('The add method ', function(t){
     var actual = add(1, 2);
     var expected = 3;
-    t.equal(actual, expected);
+    t.equal(actual, expected, 'shoud add two numbers correctly');
     t.end();
 });
