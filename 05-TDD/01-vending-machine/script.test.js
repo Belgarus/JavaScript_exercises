389b0f976e25ea2f90f69f8e3f947673dfe7f7be
Belgarus <179312058+Belgarus@users.noreply.github.com>
2025-07-10 20:38:19 +0200

    Added the missing simicolon

--- 05-TDD/01-vending-machine/script.test.js
+++ 05-TDD/01-vending-machine/script.test.js
@@ -13,7 +13,7 @@ test('getChange(486, 600) should equal [100, 10, 2, 2]', function(assert) {
   var result = getChange(486, 600);
   var expected = [100, 10, 2, 2];
   assert.deepEqual(result, expected);
-})
+});
 
 test('getChange(12, 400) should return [200, 100, 50, 20, 10, 5, 2, 1]', function(assert) {
   var result = getChange(12, 400);
