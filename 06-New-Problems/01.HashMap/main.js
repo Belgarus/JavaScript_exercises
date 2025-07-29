22191301b17b7fead3d65d6788461fe0f5b5f2a4
Belgarus <179312058+Belgarus@users.noreply.github.com>
2025-07-29 11:31:59 +0200

    added hash and set functions to the hash class

--- 06-New-Problems/01.HashMap/main.js
+++ 06-New-Problems/01.HashMap/main.js
@@ -1,6 +1,17 @@
 class HashMap {
     constructor(tablesize = 16){
         this.table = new Array(this.tablesize);
-        this.tableSize = tablesize
-    }
-}
+        this.tableSize = tablesize;
+    } 
+
+hash(key) {
+    let hashValue = 0;
+    for (let i = 0; i < key.length; i++){
+        hashValue = (hashValue+ key.charCodeAt(i)* i % this.tableSize);
+    } return hashValue
+};
+
+set(key, value){
+    const index= this.hash(key);
+    this.table[index] = value;
+}};
