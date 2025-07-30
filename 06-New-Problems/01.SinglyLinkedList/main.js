30ab03e8bead591e86a18196c23371e0bac814ed
Belgarus <179312058+Belgarus@users.noreply.github.com>
2025-07-30 13:39:56 +0200

    added a function to add nodes if the list is empty

--- 06-New-Problems/01.SinglyLinkedList/main.js
+++ 06-New-Problems/01.SinglyLinkedList/main.js
@@ -1,11 +1,21 @@
  class ListNode{
     constructor(val){
         this.val = val;
-        this.next = null;
+        this.next = null; 
     }
  }
  class LinkedList {
         head = null;
+        insert(val){
+            if(this.head === null){
+                 this.head = new ListNode(val);
+            } else {
+                
+            }
+        }
+        isEmpty(){
+            return this.head === null;
+        }
  }
 
  const list = new LinkedList();
