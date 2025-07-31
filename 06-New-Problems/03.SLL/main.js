bb21663cba1e16c80bbe9a9cdc07dd2d2429788d
Belgarus <179312058+Belgarus@users.noreply.github.com>
2025-07-31 13:26:27 +0200

    finished the append() function

--- 06-New-Problems/03.SLL/main.js
+++ 06-New-Problems/03.SLL/main.js
@@ -10,4 +10,16 @@ class LinkedList{
     constructor(value){
         this.head = null;
     };
+    append(value){
+        let newNode = new Node(value);
+        if(!this.head){
+            this.head = newNode;
+            return;
+        };
+        let current = this.head;
+        while(current.next){
+            current = current.next;
+        }
+        current.next = newNode;
+    }
 };
