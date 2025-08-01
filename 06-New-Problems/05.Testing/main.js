180ec1d0f094669eff3866210403ee6538dba8c8
Belgarus <179312058+Belgarus@users.noreply.github.com>
2025-08-01 12:22:06 +0200

    finished testing

--- 06-New-Problems/05.Testing/main.js
+++ 06-New-Problems/05.Testing/main.js
@@ -4,6 +4,10 @@ class Person {
             this.age = age;
     }
 }
+function Person2(name, age) {
+  this.name = name;
+  this.age  = age;
+}
 
 class Node {
     constructor(value){
@@ -13,5 +17,13 @@ class Node {
 }
 
 const p = new Person("Sheldon", 14)
+const p2 = new Person2("Leonard", 14)
 const n = new Node(42)
-console.log(p, n)
+console.log(p, p2, n)
+
+function Foo(bar) {
+  console.log(this);      // => das frisch erzeugte obj
+  this.bar = bar;
+}
+const f = new Foo(123);
+console.log(f.bar); 
