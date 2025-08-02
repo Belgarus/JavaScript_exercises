db1d83e81d4d5869df0e86f568fe4d03fb9c1734
Belgarus <179312058+Belgarus@users.noreply.github.com>
2025-08-02 12:21:43 +0200

    fixed a wrong variable name

--- 06-New-Problems/04.SLL/main.js
+++ 06-New-Problems/04.SLL/main.js
@@ -5,6 +5,27 @@ class Node{
     }
 }
 
-function LinkedList(data){
-    
+class LinkedList{
+    constructor(data){
+        this.head = null;
+    }
+    append(data){
+        let newNode = new Node(data);
+        if(this.head = null){
+            this.head = newNode();
+            return;
+        }
+
+    }
+    printList(){
+
+    }
 }
+
+/*let head = new Node(1);
+head.next = new Node(2);
+head.next.next = new Node(3);
+console.log(head.value, head.next.value, head.next.next.value); */
+let list = new LinkedList();
+list.append("uno")
+console.log(list)
