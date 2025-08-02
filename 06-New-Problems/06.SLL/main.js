a1f59435d5fd9ffa956004022f96e48c32d4a07d
Belgarus <179312058+Belgarus@users.noreply.github.com>
2025-08-02 13:39:27 +0200

    wrote append function to add a Node with value if the list is empty

--- 06-New-Problems/06.SLL/main.js
+++ 06-New-Problems/06.SLL/main.js
@@ -11,6 +11,15 @@ class LinkedList{
     }
     append(value){
         let newNode = new Node(value);
-        
+        let current = this.head;
+        if(this.head === null){
+            this.head = newNode;
+            newNode.next = null
+            return;
+        }
     }
 }
+
+let list = new LinkedList;
+list.append("1")
+console.log(list)
