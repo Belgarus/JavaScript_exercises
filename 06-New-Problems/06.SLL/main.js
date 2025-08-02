da16a7216f271fd115a57fdb3ce86edd5488d06a
Belgarus <179312058+Belgarus@users.noreply.github.com>
2025-08-02 13:33:26 +0200

    added the basics for my linked List

---
+++ 06-New-Problems/06.SLL/main.js
@@ -0,0 +1,16 @@
+class Node {
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkedList{
+    constructor(value){
+        this.head = null;
+    }
+    append(value){
+        let newNode = new Node(value);
+        
+    }
+}
