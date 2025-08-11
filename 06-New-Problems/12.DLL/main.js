2979943d9392e88061f61b170ea53fc3a20caaca
Belgarus <179312058+Belgarus@users.noreply.github.com>
2025-08-11 10:34:03 +0200

    added a append function for when the list is empty

--- 06-New-Problems/12.DLL/main.js
+++ 06-New-Problems/12.DLL/main.js
@@ -12,4 +12,11 @@ class DoublyLinkedList{
         this.tail = null;
         this.length = 0;
     }
+    append(value){
+        let newNode = new Node(value);
+        if(!this.head){
+            this.head = newNode;
+            this.tail = newNode;
+        }
+    }
 }
