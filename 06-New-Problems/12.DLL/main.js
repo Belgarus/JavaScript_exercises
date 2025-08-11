7fc69ad9a52680b0ccc6924ffb6fb4d0990044ed
Belgarus <179312058+Belgarus@users.noreply.github.com>
2025-08-11 10:50:20 +0200

    fixed the append function

--- 06-New-Problems/12.DLL/main.js
+++ 06-New-Problems/12.DLL/main.js
@@ -17,18 +17,23 @@ class DoublyLinkedList{
         if(!this.head){
             this.head = newNode;
             this.tail = newNode;
-        }
+        } else {
+            newNode.prev = this.tail;
+            this.tail.next = newNode;
+            this.tail = newNode;
+        } this.length++;
     }
     print(value){
         let current = this.head;
         let res = 'head -> ';
         while(current){
-            res += `${current.value} <->`;
+            res += `${current.value} <-> `;
             current = current.next;
-        } console.log(`${res} null`)
+        } console.log(`${res}null`)
     }
 }
 
 let list = new DoublyLinkedList();
 list.append(1);
+list.append(2);
 list.print();
