d09bd42f6c5671bc8cad9e51d3619fa97a918ffe
Belgarus <179312058+Belgarus@users.noreply.github.com>
2025-08-18 13:31:01 +0200

    first attempt of bulding a bigestSum function

--- 02-JavaScript-basics/01-TOP-functions/main.js
+++ 02-JavaScript-basics/01-TOP-functions/main.js
@@ -5,3 +5,12 @@ const arr = [12, 213, 21, 2, 189, 23, 190];
 let arrFnc = arr.reduce((a, b) => a + b, 0);
 
 console.log(arrFnc);
+
+function bigestSum(Arr){
+    let res = 0;
+    for(let i = 0; i < Arr.length -1; i++){
+        let sum = Arr[i] + Arr[i + 1];
+        if(sum > res){res = sum};
+    } return res;
+}
+console.log(bigestSum(arr))
