f37f6ca84bdc3d814533ad5590f318d848c81e76
Belgarus <179312058+Belgarus@users.noreply.github.com>
2025-08-18 13:47:15 +0200

    added a good sum function

--- 02-JavaScript-basics/01-TOP-functions/main.js
+++ 02-JavaScript-basics/01-TOP-functions/main.js
@@ -3,14 +3,9 @@ let sum = (a, b) => a + b;
 const arr = [12, 213, 21, 2, 189, 23, 190];
 
 let arrFnc = arr.reduce((a, b) => a + b, 0);
-
 console.log(arrFnc);
 
-function bigestSum(Arr){
-    let res = 0;
-    for(let i = 0; i < Arr.length -1; i++){
-        let sum = Arr[i] + Arr[i + 1];
-        if(sum > res){res = sum};
-    } return res;
-}
-console.log(bigestSum(arr))
+let newArr = arr.sort((a, b) => a - b)
+let len = newArr.length
+
+console.log(newArr[len - 1] + newArr[len - 2]);
