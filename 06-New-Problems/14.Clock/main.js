96b41866a0c89158697c4a558eac56871f5ac48c
Belgarus <Club.Mate@tutamail.com>
2025-10-29 19:37:05 +0100

    added the timer

--- 06-New-Problems/14.Clock/main.js
+++ 06-New-Problems/14.Clock/main.js
@@ -1,6 +1,21 @@
-//When do you want to set the timer?
-//Input 
+const readline = require('readline');
 
-//Check Time, if time = Input 
-// -> Alarm
-// Stop Alarm 
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Time (HH:MM): ', (time) => {
+    const [hours, minutes] = time.split(':');
+    
+    console.log(`Alarm set for ${time}`);
+    
+    const check = setInterval(() => {
+        const now = new Date();
+        if (now.getHours() == hours && now.getMinutes() == minutes) {
+            console.log('\n🔔 ALARM!!!');
+            clearInterval(check);
+            rl.close();
+        }
+    }, 1000);
+});
